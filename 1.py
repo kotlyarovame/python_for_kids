8af9505b4f5ae8133a1c106230e3982ea0facd8f
Marina Kotlyarova <kotlyarova.me@gmail.com>
2019-05-03 20:11:22 +0300

    вычисления + задачка про монетки

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
 print ("hello world")
+
+#пробую вычисления
+count = 35 * "ах"
+print(count)
+
+#задачка про монетки
+found_coins = 20
+magic_coins = 13
+stolen_coins = 3
+print (found_coins + magic_coins * 365 - stolen_coins *52)
